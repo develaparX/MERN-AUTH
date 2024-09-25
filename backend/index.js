e2e5e3b868c87eb8652b217bb40f1a86eaac3cdd
arunikax <srarfian@gmail.com>
2024-09-25 22:54:08 +0700

    signUp -> with email token don

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,23 @@
 import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.routes.js";
+import { connectDB } from "./db/connectDB.js";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); //allows us to parse incoming requests:req.body
 
 app.get("/", (req, res) => {
   res.send("Hello World! 2");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.use("/api/auth", authRoutes);
+
+app.listen(5000, () => {
+  connectDB();
+  console.log("Server is running on port:", PORT);
 });
